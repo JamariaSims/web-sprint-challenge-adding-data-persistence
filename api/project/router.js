44c2fddd626d99725c18395d5f9df472a59700cd
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-10 01:49:07 -0400

    basic end points

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,7 @@
 // build your `/api/projects` router here
+const express = require("express");
+const router = express.Router();
+
+server.use(express.json());
+server.use("/api/projects");
+module.exports = server;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
-// build your server here and require it from index.js
+const express = require("express");
+const router = require("./project/router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/projects", router);
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
